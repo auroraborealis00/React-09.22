14d61d2baf75ab43ebf64dafa481c110a36d0288
auroraborealis00 <112113295+auroraborealis00@users.noreply.github.com>
2022-10-19 20:36:15 +0300

    kujundus, fetch

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import { useRef } from "react";
 import styles from "../css/Cart.module.css";
 
 
 function Cart() {
     const [cart, renewCart] = useState( JSON.parse(localStorage.getItem("cart")) || [] );
+    const [parcelMachines, setParcelMachines] = useState([]);
+    const pmRef = useRef();
+    
+
+    useEffect(() => {
+fetch("https://www.omniva.ee/locations.json")
+.then(res => res.json()) // res tähendab response //
+.then(json => setParcelMachines(json))
+    }, []);
 
     const del = (index) => {
         cart.splice(index,1);
@@ -32,6 +43,12 @@ return sum.toFixed(2);
         localStorage.setItem("cart", JSON.stringify(cart));
 
     }
+
+    const sendOrder = () => {
+        console.log(pmRef.current.value);
+        console.log(cart);
+    }
+
     return ( <div>
         
         { cart.length > 0 && <div> Cart includes {cart.length} items</div> }
@@ -43,13 +60,26 @@ return sum.toFixed(2);
             <img className={styles.image} src={element.product.image} alt="" />
             <div className={styles.name}>{element.product.name}</div>
             <div className={styles.price}>{element.product.price} €</div>
-            <button onClick={() => decreaseQuantity(index)}>-</button>
-            <div className={styles.quantity}>{element.quantity} tk</div>
-            <button onClick={() => increaseQuantity(index)}>+</button>
+            <div className={styles.quantity}>
+            <img className={styles.button} onClick={() => decreaseQuantity(index)} src={require("../images/minus.png")} alt="" />
+            <div>{element.quantity} tk</div>
+            <img className={styles.button} onClick={() => increaseQuantity(index)} src={require("../images/plus.png")} alt="" />
+            </div>
             <div className={styles.sum}>{ (element.product.price * element.quantity).toFixed(2) } €</div>
-            <button onClick={() => del(index)}>x</button>
+            <img className={styles.button} onClick={() => del(index)} src={require("../images/close.png")} alt="" />
         </div>)}
-        <div>Sum: {calculateCartSum()} €</div>
+
+        <div className={styles.cart__bottom}>
+            <div>Sum: {calculateCartSum()} €</div>
+
+<select ref={pmRef}>{parcelMachines
+    .filter(element => element.A0_NAME === "EE")
+    .map(element => 
+    <option key={element.NAME}>{element.NAME}</option>)}
+    </select>
+
+<button onClick={sendOrder}>Vormista tellimus</button>
+</div>
     </div>  );
 }
 
